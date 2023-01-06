82d598104f0a59cd26d4b74b427504a268a76527
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2023-01-06 13:52:35 +0530

    Update function-calling.py

--- function-calling.py
+++ function-calling.py
@@ -3,7 +3,8 @@
 calculate = 24
 name_of_unit = "hours"
 
-def days_to_units():
-    print(f"20 Days are {20 * calculate} {name_of_unit}")
+def days_to_units(num_of_days):
+    print(f"{num_of_days} Days are {num_of_days * calculate} {name_of_unit}")
 
-days_to_units()
+days_to_units(20)
+days_t0_units(35)
