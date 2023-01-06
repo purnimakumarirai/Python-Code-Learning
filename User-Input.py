42f4016220df121e23a4a0aca5b74e7f428563f9
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2023-01-06 14:36:47 +0530

    Create User-Input.py

---
+++ User-Input.py
@@ -0,0 +1,8 @@
+calculate = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} Days are {num_of_days * calculate} {name_of_unit}")
+
+user_input = input("Hey user, Enter a number and will convert it into hours!\n")
+print(user_input)
