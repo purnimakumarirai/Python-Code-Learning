95ed4d462e300844422b46cf30dc69979d2757de
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2023-01-08 17:08:46 +0530

    Update nested.py

--- nested.py
+++ nested.py
@@ -17,3 +17,24 @@ user_input_num=int(user_input)
 
 calculated_value = days_to_units(user_input_num)
 print(calculated_value)
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+
+calculate = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+        return f"{num_of_days} Days are {num_of_days * calculate} {name_of_unit}"
+
+
+def validate_and_execute():
+    if user_input.digit():
+        user_input_num = int(user_input)
+        if user_input_num > 0:
+            calculated_value = days_to_units(user_input_num)
+            print(calculated_value)
+        elif user_input_num == 0:
+            print("you entered a 0, please enter a valid +ve number")
+    else:
+        print("your input is not valid number, don't ruin my code")
