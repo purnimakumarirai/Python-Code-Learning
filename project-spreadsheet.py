e55f1d77584eccc1abf39aec690911b67d922e2d
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2023-01-10 14:50:29 +0530

    Create project-spreadsheet.py

---
+++ project-spreadsheet.py
@@ -0,0 +1,59 @@
+import openpyxl
+#openpyxl is a python library to read/write Excel file
+
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+#inv_file is file and workbook for implemention
+
+product_list = inv_file["Sheet1"]
+#variable containing the file information
+
+#calculation the sheet info below in empty{} dictionary
+products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
+
+#loop
+for product_row in range(2, product_list.max_row + 1):
+  
+    supplier_name = product_list.cell(product_row, 4).value
+    
+    inventory = product_list.cell(product_row, 2).value
+    
+    price = product_list.cell(product_row, 3).value
+    
+    product_num = product_list.cell(product_row, 1).value
+    
+    inventory_price = product_list.cell(product_row, 5)
+
+    # calculation number of products per supplier
+    if supplier_name in products_per_supplier:
+      
+        current_num_products = products_per_supplier.get(supplier_name)
+        
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
+
+    # calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+      
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+    # logic products with inventory less than 10
+    if inventory < 10:
+      
+        products_under_10_inv[int(product_num)] = int(inventory)
+
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
+
+print(products_per_supplier)
+print(total_value_per_supplier)
+print(products_under_10_inv)
+
+inv_file.save("inventory_with_total_value.xlsx")
