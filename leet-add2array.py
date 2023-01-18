07ed68bc9f45bdaf78d51e1b54b9466fe1a4784e
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2023-01-18 18:55:49 +0530

    Create leet-add2array.py

---
+++ leet-add2array.py
@@ -0,0 +1,23 @@
+class Solution:
+  
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+      
+        for i in range(len(nums)):
+          
+            for j in range(i + 1, len(nums)):
+              
+                if nums[i] + nums[j] == target:
+                  
+                    return [i, j]
+        return []
+      
+--------------------------------------------------------------------------------------------------------------------------------------------------      
+class Solution:
+  def twosum(self, nums:[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+      for j in range(i+1, len(nums)):
+        if nums[i] +nums[j] == target:
+          return [i+j]
+        return []
+  
+      
